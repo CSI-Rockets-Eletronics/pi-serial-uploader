fd340f26f86f3dece8ac861cb83ed6fe5cfd1fb8
Alex Chen <axchen7@outlook.com>
2025-03-05 03:09:33 -0500

    Update main_scientific for start of 2025

--- main_scientific.py
+++ main_scientific.py
@@ -6,16 +6,24 @@ delimiter = b"\xAA\x55"  # {0b10101010, 0b01010101}
 
 
 def parse_packet(packet: bytes) -> str:
-    if len(packet) == 16:
-        # breakdown of "!qii":
-        #   "!": network byte order
-        #   "q": long long
-        #   "i": int
-        ts, st1, st2 = struct.unpack("!qii", packet)
-        data = {"ts": ts, "st1": st1, "st2": st2}
+    if len(packet) == 24:
+        # breakdown of "<Qffff":
+        #   "<": little-endian
+        #   "Q": uint64_t (8 bytes)
+        #   "f": float (4 bytes)
+        ts, lox_upper, lox_lower, gn2_manifold_1, gn2_manifold_2 = struct.unpack(
+            "<Qffff", packet
+        )
+        data = {
+            "ts": ts,
+            "lox_upper": lox_upper,
+            "lox_lower": lox_lower,
+            "gn2_manifold_1": gn2_manifold_1,
+            "gn2_manifold_2": gn2_manifold_2,
+        }
         return json.dumps(data)
 
-    raise ValueError(f"Expected packet length 16, got {len(packet)}")
+    raise ValueError(f"Expected packet length 24, got {len(packet)}")
 
 
 uploader.run("Scientific", delimiter, parse_packet)
