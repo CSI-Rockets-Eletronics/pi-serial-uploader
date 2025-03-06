65c99c6210e6835e119a857d97e90c2f80d7ca41
Alex Chen <axchen7@outlook.com>
2025-03-06 00:50:20 -0500

    Handle thermocouples

--- main_fs_pi.py
+++ main_fs_pi.py
@@ -7,9 +7,11 @@ delimiter = b"\xaa\x55"  # {0b10101010, 0b01010101}
 
 def parse_device(packet: bytes) -> str:
     if len(packet) == 24:
-        return "Scientific1"
+        return "FsLoxGn2Transducers"
     if len(packet) == 16:
-        return "Scientific2"
+        return "FsInjectorTransducers"
+    if len(packet) == 17:
+        return "FsThermocouples"
     raise ValueError(f"Invalid packet length: {len(packet)}")
 
 
@@ -41,6 +43,20 @@ def parse_packet(packet: bytes) -> str:
             "ts": ts,
             "injector_manifold_1": injector_manifold_1,
             "injector_manifold_2": injector_manifold_2,
+        }
+        return json.dumps(data)
+
+    if len(packet) == 17:
+        # breakdown of "<QffB":
+        #   "<": little-endian
+        #   "Q": uint64_t (8 bytes)
+        #   "f": float (4 bytes)
+        #   "B": uint8_t (1 byte)
+        ts, lox_celsius, gn2_celsius, _dummy = struct.unpack("<QffB", packet)
+        data = {
+            "ts": ts,
+            "lox_celsius": lox_celsius,
+            "gn2_celsius": gn2_celsius,
         }
         return json.dumps(data)
 
