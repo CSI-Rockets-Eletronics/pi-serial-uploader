02078aa5ac3784b5621a3856fb6811f5916b05c0
Alex Chen <axchen7@outlook.com>
2025-03-06 13:00:39 -0500

    Implement format_message

--- main_fs_pi.py
+++ main_fs_pi.py
@@ -1,8 +1,26 @@
 import struct
+from enum import Enum
+from typing import Any
 
 import uploader
 
 delimiter = b"\xaa\x55"  # {0b10101010, 0b01010101}
+
+
+# values must be between 0 and 255 (1 byte)
+class FsCommands(Enum):
+    STATE_ABORT = 0
+    STATE_STANDBY = 1
+    STATE_GN2_STANDBY = 2
+    STATE_GN2_FILL = 10
+    STATE_GN2_PULSE_FILL_A = 11
+    STATE_GN2_PULSE_FILL_B = 12
+    STATE_GN2_PULSE_FILL_C = 13
+    STATE_FIRE = 20
+    STATE_FIRE_MANUAL_DOME_PILOT_OPEN = 21
+    STATE_FIRE_MANUAL_DOME_PILOT_CLOSE = 22
+    STATE_FIRE_MANUAL_IGNITER = 23
+    STATE_FIRE_MANUAL_RUN = 24
 
 
 def parse_packet(packet: bytes):
@@ -51,4 +69,16 @@ def parse_packet(packet: bytes):
         return uploader.Record("FsThermocouples", data)
 
 
-uploader.run(delimiter, parse_packet)
+def format_message(message: Any):
+    command = message["command"]
+    assert isinstance(command, str)
+
+    try:
+        command_value = FsCommands[command].value
+    except KeyError:
+        raise ValueError(f"Invalid command: {command}")
+
+    return struct.pack("<B", command_value) + delimiter
+
+
+uploader.run(delimiter, parse_packet, {"FiringStation": format_message})
