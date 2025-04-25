29f944774cb0f972f0bd6c0e4f3133108283bc56
Alex Chen <axchen7@outlook.com>
2025-04-25 03:30:26 -0400

    update serial device for rpi 5

--- uploader.py
+++ uploader.py
@@ -13,7 +13,7 @@ MAX_RECORDS_PER_BATCH = 500
 URL = "http://localhost:3000"
 ENVIRONMENT_KEY = "0"
 
-ser = serial.Serial("/dev/ttyS0", 230400)
+ser = serial.Serial("/dev/serial0", 230400)
 
 
 @dataclass
